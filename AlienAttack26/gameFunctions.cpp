611001eae699189555cc140276d2e6fe7f18ebc7
rlp81 <58034254+rlp81@users.noreply.github.com>
2026-08-05 20:06:27 -0500

    Clean up author comments in gameFunctions.cpp
    Removed author comments from gameFunctions.cpp

--- AlienAttack26/gameFunctions.cpp
+++ AlienAttack26/gameFunctions.cpp
@@ -2,11 +2,6 @@
 /*
 *  gameFunctions.cpp
 *     - The functions for the game program are in this file. 
-* 
-*  AUTHOR: Most of the work in this program was completed by Prof. Andy Harbert
-*          Prof. Pat Smith has made modifications (December 2021)
-*		   Cole lehl
-*
 */
 
 // Declare Global Variables
